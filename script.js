d7d8830a187809284148451efd8134c554ae8d98
prasanth-raj-sankar <prasanthraj0910@gmail.com>
2024-04-13 18:47:55 +0530

    day-3-13-4-24

---
+++ script.js
@@ -0,0 +1,37 @@
+//XML-HTTP request(XHR)
+//they are used to interact with the server via api
+
+
+//step 01:
+//create a XHR object
+var request = new XMLHttpRequest()
+
+//step 02:
+//opening a request
+//this method tells us that which Http method as well as which API
+request.open("GET","https://restcountries.com/v3.1/all")
+
+
+//step 03:
+//kickstart the HTTP Operation
+
+request.send()
+
+//there the JSON.parse converts the JSON string into array of object
+
+request.onload = function(){
+
+    //here the conversion of string to object
+
+    var res = JSON.parse(request.response)
+    console.log(res);
+
+//print the regions of all 250 coutries
+  
+for(var i=0;i<res.length;i++)
+{
+  console.log(res[i].region)
+}
+
+
+}
